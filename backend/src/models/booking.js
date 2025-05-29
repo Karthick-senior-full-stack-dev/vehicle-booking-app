e0d70b43e5943e1d52fa3dfb23ee3a5581163f0c
Karthickkumar <karthickengineer007@gmail.com>
2025-05-29 21:17:36 +0530

    Created the bookings models and migration

---
+++ backend/src/models/booking.js
@@ -0,0 +1,23 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    static associate(models) {
+      Booking.belongsTo(models.Vehicle, {
+        foreignKey: 'vehicleId',
+        as: 'vehicle'
+      });
+    }
+  }
+  Booking.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    vehicleId: DataTypes.INTEGER,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
